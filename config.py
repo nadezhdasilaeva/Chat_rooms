f8640ed46de095c3b30bcb6d785a9bec15ac210a
nadezhdasilaeva <147171588+nadezhdasilaeva@users.noreply.github.com>
2025-07-11 17:41:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,7 @@ load_dotenv()
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 ALGORITHM = os.environ.get("ALGORITHM")
+ENCRYPTION_KEY = os.environ.get("ENCRYPTION_KEY")
 
-ENCRYPTION_KEY = os.environ.get("ENCRYPTION_KEY")   
+PASS_ADMIN = os.environ.get("PASS_ADMIN")
+EMAIL_ADMIN = os.environ.get("EMAIL_ADMIN") 
