77fa4cd4ecd2faa7c47e1b0d48aa90f506cfeb65
nadezhdasilaeva <147171588+nadezhdasilaeva@users.noreply.github.com>
2025-07-11 17:42:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import uvicorn
 from sqlmodel import SQLModel
 
 from db import engine
-from routers import chat, user
+from routers import chat, user, admin
 
 from web import input as web_input
 from web import register as web_register
 from web import login as web_login
 from web import chat as web_chat
 from web import users as web_users
+from web import admins as web_admins
 
 
 if __name__ == '__main__':
@@ -20,6 +21,7 @@ app = FastAPI()
 
 app.include_router(user.router)
 app.include_router(chat.router)
+app.include_router(admin.router)
 
 
 app.include_router(web_input.router)
@@ -27,6 +29,7 @@ app.include_router(web_register.router)
 app.include_router(web_login.router)
 app.include_router(web_chat.router)
 app.include_router(web_users.router)
+app.include_router(web_admins.router)
 
 
 if __name__ == '__main__':
